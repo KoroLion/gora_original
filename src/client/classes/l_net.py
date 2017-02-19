71c10641d2fa7103376b47fb30a35d3e00e95bd7
KoroLion <korolion31@yandex.ru>
2017-02-19 14:33:46 +0300

    Улучшение кода

---
+++ src/client/classes/l_net.py
@@ -0,0 +1,24 @@
+class LNet(object):
+    """
+    Класс для простой работы с сетью
+    """
+    def __init__(self, ip, port, timeout):
+        self.ip = ip
+        self.port = port
+        self.timeout = timeout
+
+    def tcp_send(self, data):
+        # IPv4, потокориентированный, TCP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
+        sock.connect((self.ip, self.port))
+        sock.sendall(data)
+        data = sock.recv(PACKET_SIZE)
+        sock.close()
+        return data
+
+    def udp_send(self, data):
+        # IPv4, датаграммный, UDP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect((self.ip, self.port))
+        sock.sendall(data)
+        sock.close()
--- src/client/main.py
+++ src/client/main.py
@@ -12,6 +12,8 @@ from classes.resources import Resources
 from classes.core import Core
 from classes.game import Game
 
+from classes.l_net import LNet
+
 PACKET_SIZE = 512
 
 IP = '195.170.57.185'
@@ -50,32 +52,6 @@ class PlayerInfo(object):
     def update(self):
         self.position.x += self.speed.x
         self.position.y += self.speed.y
-
-
-class LNet(object):
-    """
-    Класс для простой работы с сетью
-    """
-    def __init__(self, ip, port, timeout):
-        self.ip = ip
-        self.port = port
-        self.timeout = timeout
-
-    def tcp_send(self, data):
-        # IPv4, потокориентированный, TCP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
-        sock.connect((self.ip, self.port))
-        sock.sendall(data)
-        data = sock.recv(PACKET_SIZE)
-        sock.close()
-        return data
-
-    def udp_send(self, data):
-        # IPv4, датаграммный, UDP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.connect((self.ip, self.port))
-        sock.sendall(data)
-        sock.close()
 
 pygame.init()
 
