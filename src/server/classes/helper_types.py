e66b28486a9871bbc09359e0433a76f7190fb566
KoroLion <korolion31@yandex.ru>
2017-02-22 23:38:15 +0300

    Улучшения кода

--- src/server/classes/helper_types.py
+++ src/server/classes/helper_types.py
@@ -3,36 +3,29 @@
 
 
 class Point(object):
-    """Class for storing position data."""
+    """!
+    @brief хранение данных с x и y
+    """
 
     def __init__(self, x: int, y: int):
-        """
-        Initialize Position object
+        """!
         :type x: int
         :type y: int
         """
         self.x = x  # pylint: disable=invalid-name
         self.y = y  # pylint: disable=invalid-name
 
-class Position(object):
-    """Class for storing position data."""
-
-    def __init__(self, x, y):
-        """
-        Initialize Position object
-        :type x: int
-        :type y: int
-        """
-        self.x = x  # pylint: disable=invalid-name
-        self.y = y  # pylint: disable=invalid-name
+    def __eq__(self, other):
+        return (self.x == other.x) and (self.x == other.x)
 
 
 class Size(object):
-    """Class for storing size data."""
+    """!
+    @brief хранение данных с шириной и выстой
+    """
 
     def __init__(self, width, height):
-        """
-        Initialize Size object
+        """!
         :type width: int
         :type height: int
         """
@@ -41,19 +34,3 @@ class Size(object):
 
     def __eq__(self, other):
         return (self.width == other.width) and (self.height == other.height)
-
-
-class Speed(object):
-    """Class for storing speed data."""
-
-    def __init__(self, x, y):
-        """
-        Initialize Speed object
-        :type x: int
-        :type y: int
-        """
-        self.x = x  # pylint: disable=invalid-name
-        self.y = y  # pylint: disable=invalid-name
-
-if __name__ == "__main__":
-    pass
