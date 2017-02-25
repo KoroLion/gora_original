daf75528167a49ab037c5ef6ad674e70cfedb582
KoroLion <korolion31@yandex.ru>
2017-02-26 02:58:51 +0300

    Улучшения кода

--- src/client/classes/l_net.py
+++ src/client/classes/l_net.py
@@ -19,13 +19,12 @@ class LNet(object):
         # IPv4, потокориентированный, TCP
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
         sock.connect((self.ip, self.port))
-        data += '\n'
         sock.sendall(data.encode())
         data = sock.recv(PACKET_SIZE)
         sock.close()
         return data
 
-    def tcp_send_(self, data: str, loop) -> str:
+    def tcp_send_async(self, data: str, loop) -> str:
         @asyncio.coroutine
         def tcp_echo_client(message, loop):
             reader, writer = yield from asyncio.open_connection(self.ip, self.port, loop=loop)
@@ -42,55 +41,7 @@ class LNet(object):
         loop.close()
         return data
 
-    def tcp_send_(self, data: str) -> str:
-        def tcp_echo_client(message):
-            reader, writer = asyncio.open_connection(self.ip, self.port)
-
-            writer.write(message.encode())
-            writer.write_eof()
-
-            recv = reader.read()
-            writer.close()
-
-            return recv.decode()
-
-        return tcp_echo_client(data)
-
     def udp_send(self, data: str) -> str:
-        loop = asyncio.new_event_loop()
-
-        class EchoClientProtocol(asyncio.DatagramProtocol):
-            def __init__(self, message, loop):
-                self.message = message
-                self.loop = loop
-                self.transport = None
-                self.received = ''
-
-            def connection_made(self, transport):
-                self.transport = transport
-                self.transport.sendto(self.message.encode())
-
-            def datagram_received(self, data, addr):
-                self.received = data.decode()
-                self.transport.close()
-
-            def error_received(self, exc):
-                print('Error received:', exc)
-
-            def connection_lost(self, exc):
-                self.loop.stop()
-
-        connect = loop.create_datagram_endpoint(
-            lambda: EchoClientProtocol(data, loop),
-            remote_addr=(self.ip, self.port))
-        transport, protocol = loop.run_until_complete(connect)
-        loop.run_forever()
-        transport.close()
-        loop.close()
-        print('loop_closed')
-        return protocol.received
-
-    def udp_send_(self, data: str) -> str:
         # IPv4, датаграммный, UDP
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.connect((self.ip, self.port))
