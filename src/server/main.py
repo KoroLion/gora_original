6b375665517cf055741e7d298b850ea282991f43
KoroLion <korolion31@yandex.ru>
2017-02-27 21:26:52 +0300

    Удалена многопоточность

--- src/server/main.py
+++ src/server/main.py
@@ -1,6 +1,5 @@
+import sys
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
-from time import sleep
 import ujson as json
 
 from classes.helper_types import Point
@@ -52,98 +51,78 @@ class PlayerInfo:
         self.position.y += self.speed.y
 
 
-def tcp_async(loop):
-    @asyncio.coroutine
-    def tcp_handle(reader, writer):
-        while True:
-            data = yield from reader.read(1024)  # read(100) - читать 100 байт, иначе до EOF
-            if not data:
-                break
+async def tcp_handle(reader, writer):
+    while True:
+        data = await reader.read(1024)  # read(100) - читать 100 байт, иначе до EOF
+        if not data:
+            break
 
-            addr = writer.get_extra_info('peername')
+        addr = writer.get_extra_info('peername')
 
-            data = data.decode()
-            try:
-                data = json.loads(data)
-                correct_data = True
-            except json:
-                print('#ERROR: incorrect format of input data')
-                correct_data = False
+        data = data.decode()
+        try:
+            data = json.loads(data)
+            correct_data = True
+        except json:
+            print('#ERROR: incorrect format of input data')
+            correct_data = False
 
-            if correct_data:
-                command = data.get(J_COMMAND)
-                token = data.get(J_TOKEN)
-                players = server.players
-                cur_player_token = token
+        if correct_data:
+            command = data.get(J_COMMAND)
+            token = data.get(J_TOKEN)
+            players = server.players
+            cur_player_token = token
 
-                if data.get(J_COMMAND) == CONNECT:
-                    print(token + ' (' + addr[0] + ') connected!')
-                    new_player = {token: PlayerInfo(Point(100, 100), Point(0, 0),
-                                                    ip=addr[0],
-                                                    skin=data.get(J_SKIN),
-                                                    angle=data.get(J_ANGLE))
-                                  }
-                    players.update(new_player)
-                    writer.write('OK'.encode())
-                elif command == GO_TOP:
-                    players[token].speed.x = 0
-                    players[token].speed.y = -players[token].speed_amount
-                    writer.write('OK'.encode())
-                elif command == GO_BOTTOM:
-                    players[token].speed.x = 0
-                    players[token].speed.y = players[token].speed_amount
-                    writer.write('OK'.encode())
-                elif command == GO_LEFT:
-                    players[token].speed.x = -players[token].speed_amount
-                    players[token].speed.y = 0
-                    writer.write('OK'.encode())
-                elif command == GO_RIGHT:
-                    players[token].speed.x = players[token].speed_amount
-                    players[token].speed.y = 0
-                    writer.write('OK'.encode())
-                elif command == GET_DATA:
-                    angle = round(data.get(J_ANGLE))
+            if data.get(J_COMMAND) == CONNECT:
+                print(token + ' (' + addr[0] + ') connected!')
+                new_player = {token: PlayerInfo(Point(100, 100), Point(0, 0),
+                                                ip=addr[0],
+                                                skin=data.get(J_SKIN),
+                                                angle=data.get(J_ANGLE))
+                              }
+                players.update(new_player)
+                writer.write('OK'.encode())
+            elif command == GO_TOP:
+                players[token].speed.x = 0
+                players[token].speed.y = -players[token].speed_amount
+                writer.write('OK'.encode())
+            elif command == GO_BOTTOM:
+                players[token].speed.x = 0
+                players[token].speed.y = players[token].speed_amount
+                writer.write('OK'.encode())
+            elif command == GO_LEFT:
+                players[token].speed.x = -players[token].speed_amount
+                players[token].speed.y = 0
+                writer.write('OK'.encode())
+            elif command == GO_RIGHT:
+                players[token].speed.x = players[token].speed_amount
+                players[token].speed.y = 0
+                writer.write('OK'.encode())
+            elif command == GET_DATA:
+                angle = round(data.get(J_ANGLE))
 
-                    server.players[cur_player_token].angle = angle
-                    j_players = []
-                    for token in server.players:
-                        data = {J_TOKEN: token,
-                                J_POSITION_X: server.players[token].position.x,
-                                J_POSITION_Y: server.players[token].position.y,
-                                J_ANGLE: server.players[token].angle,
-                                J_SKIN: server.players[token].skin}
-                        j_players.append(data)
+                server.players[cur_player_token].angle = angle
+                j_players = []
+                for token in server.players:
+                    data = {J_TOKEN: token,
+                            J_POSITION_X: server.players[token].position.x,
+                            J_POSITION_Y: server.players[token].position.y,
+                            J_ANGLE: server.players[token].angle,
+                            J_SKIN: server.players[token].skin}
+                    j_players.append(data)
 
-                    writer.write(json.dumps(j_players).encode())
-                elif command == DISCONNECT:
-                    print(cur_player_token + ' (' + players[cur_player_token].ip + ') disconnected!')
-                    server.disconnect_player(cur_player_token)
-                    writer.write('OK'.encode())
+                writer.write(json.dumps(j_players).encode())
+            elif command == DISCONNECT:
+                print(cur_player_token + ' (' + players[cur_player_token].ip + ') disconnected!')
+                server.disconnect_player(cur_player_token)
+                writer.write('OK'.encode())
 
-        writer.close()
+    writer.close()
 
-    coro = asyncio.start_server(tcp_handle, '', PORT, loop=loop)
-    tcp_server = loop.run_until_complete(coro)
-
-    # сервер запущен, пока не нажали Ctrl+C
-    addr = tcp_server.sockets[0].getsockname()
-    print('TCP server started at {}:{}'.format(addr[0], addr[1]))
-    try:
-        loop.run_forever()
-        pass
-    except KeyboardInterrupt:
-        pass
-
-    # Close the server
-    tcp_server.close()
-    loop.run_until_complete(tcp_server.wait_closed())
-    loop.close()
-
-
-def main():
+async def main():
     while not server.terminated:
-        sleep(0.05)
         server.update_players()
+        await asyncio.sleep(0.05)
 
 if __name__ == "__main__":
     asyncio_loop = asyncio.get_event_loop()
@@ -152,9 +131,18 @@ if __name__ == "__main__":
     print('Initializing server...')
     server = ServerCore(IP, PORT)
 
-    print('Starting TCP thread...')
-    tcp_executor = ThreadPoolExecutor(max_workers=1)
-    asyncio_loop.run_in_executor(tcp_executor, tcp_async, asyncio_loop)
+    print('Setting up TCP server...')
+    coro = asyncio.start_server(tcp_handle, '', PORT, loop=asyncio_loop)
+    print('Starting TCP server at {}:{}...'.format(IP, PORT))
+    try:
+        tcp_server = asyncio_loop.run_until_complete(coro)
+    except OSError:
+        print('Error while attempting to bind on address {}:{}'.format(IP, PORT))
+        sys.exit()
 
-    print('Starting world...')
-    main()
+    # сервер запущен, пока не нажали Ctrl+C
+    addr = tcp_server.sockets[0].getsockname()
+    print('TCP server started at {}:{}'.format(addr[0], addr[1]))
+
+    asyncio_loop.run_until_complete(main())
+    asyncio_loop.close()
